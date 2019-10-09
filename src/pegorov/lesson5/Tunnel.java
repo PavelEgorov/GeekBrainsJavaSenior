71007a6839be37acc16446871e36e2c46dc43310
PavelEgorov <jonnycomein@gmail.com>
2019-10-09 20:42:00 +0300

    решение задачи 5

--- src/pegorov/lesson5/Tunnel.java
+++ src/pegorov/lesson5/Tunnel.java
@@ -1,4 +1,33 @@
 package pegorov.lesson5;
 
-public class Tunnel {
+import java.util.concurrent.Semaphore;
+
+import static pegorov.lesson5.MainClass.HALF_CARS_COUNT;
+
+public class Tunnel extends Stage {
+    Semaphore smp = new Semaphore(HALF_CARS_COUNT);
+
+    public Tunnel() {
+        this.length = 80;
+        this.description = "Тоннель " + length + " метров";
+    }
+
+    @Override
+    public void go(Car c) {
+        try {
+            try {
+                System.out.println(c.getName() + " готовится к этапу(ждет): " + description);
+                smp.acquire();
+                System.out.println(c.getName() + " начал этап: " + description);
+                Thread.sleep(length / c.getSpeed() * 1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } finally {
+                System.out.println(c.getName() + " закончил этап: " + description);
+                smp.release();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
